8009fb13083062e379d2998cd5c604d4867a11a9
jiaad <abdulziad08@outlook.com>
2019-09-30 11:50:58 +0200

    added some routes, secret, admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'admin/index'
   devise_for :users
   root to: "home#index"
+  
+  resources :users do
+    member do 
+        get :confirm_email
+    end
+  end
+
+resources :admin
+
+  #devise_for :users, controllers: { confirmations: 'confirmations' }
 end
