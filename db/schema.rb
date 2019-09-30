15dd6d6e3bec4a21bbfb98ff60ccf249756ffbfe
jiaad <abdulziad08@outlook.com>
2019-09-30 22:53:04 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_105154) do
+ActiveRecord::Schema.define(version: 2019_09_30_203605) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "time_lines", force: :cascade do |t|
+    t.string "descrition"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
