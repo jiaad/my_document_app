06c026dc7b74c4d543c5aef9534bbecf6f7e21fb
jiaad <abdulziad08@outlook.com>
2019-09-30 11:47:14 +0200

    added devise logic, confirmable, changed after_confirmation, added enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,32 @@
 class User < ApplicationRecord
-after_create :send_welcome_email
+
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  # after_save :send_welcome_email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  def send_welcome_email
-      UserMailer.welcome_email(self).deliver_now
+  def after_confirmation
+      UserMailer.welcome_email(self).deliver #if self.confirmed_at_changed?
   end
+
+  private
+  # def validate_email
+  #   self.email_confirmed = true
+  #   self.cofirm_token = nil
+  # end
+
+  # def set_confirmation_token
+  #     if self.confirm_token.blank?
+  #       self.cofirm_token = SecureRandom.urlsafe_base64.to_s
+  #     end
+  #   end
 end
