0f4bb1dd32b74ddff40f586ccf2764ed25054972
jiaad <abdulziad08@outlook.com>
2019-10-03 23:17:20 +0200

    Application:  .js , .css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -13,4 +13,14 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require bootstrap.min
+//= require wow.min
+//= require jquery.scrollTo.min
+//= require jquery.nav
+//= require modernizr.custom
+//= require grid
+//= require stellar
+//= require contactform
+//= require custom
 //= require_tree .
