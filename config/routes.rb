e10c19ad1e54a5f21f5cc184098b5198e089f611
jiaad <abdulziad08@outlook.com>
2019-10-06 22:09:20 +0200

    routes : added custom static routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ resources :profile
 resources :post do
   resources :comments#, only: [:new, :post] 
 end
+
+get 'static/news',      to: 'static#news'
+get 'static/about',     to: 'static#about'
+get 'static/contact',   to: 'static#contact'
   #devise_for :users, controllers: { confirmations: 'confirmations' }
 end
