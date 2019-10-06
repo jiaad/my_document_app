faf79678b982e9827d2d57ce87080814d1c3563f
jiaad <abdulziad08@outlook.com>
2019-10-06 21:03:09 +0200

    Javascript : deleted some functions not needed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,15 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
