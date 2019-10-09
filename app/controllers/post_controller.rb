76bcee36cc1b332e6ae6a192f9753d317f3b158d
jiaad <abdulziad08@outlook.com>
2019-10-09 12:06:39 +0200

    Image_Destroy: added image destroy method, to delete image in edit mode

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -27,12 +27,30 @@ end
 
   def update
     @post = Post.find(params[:id])
-    
+if    @post.update(post_params)
+      redirect_to post_path(@post.id)
+      else
+       puts "="*30
+        puts @post.errors.full_messages
+      puts "="*30       
+    render :edit
+    end
   end
-
+ def image_destroy
+  #@post = Post.find(params[:id])
+  #Post.find_by(id:params[:id],images: images.id ).destroy
+  #Post.images.find(params[:images_id])
+  @post = Post.find(params[:post_id])
+  #@image = ActiveStorage::Attachment.find(params[:id])
+  @image = @post.images.find(params[:image_id])
+  @image.purge
+  #@post.
+  redirect_back(fallback_location: edit_post_path(@post.id))
+ end
   protected
 
   def post_params
     params.require(:post).permit(:description, :user_id, images: [])
   end
+  alias_method :update_params, :post_params
 end
