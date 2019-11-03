6c19bb693f7cbcfb3479ac3e3b9ec724836a64ec
jiaad <abdulziad08@outlook.com>
2019-11-03 10:59:44 +0100

    TIME JS : tried to add timer but doesn't reconize <time></time>

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,19 @@
 function timer() {
-  let time = document.getElementById('time');
-  let new_time = new Time;
-  console.log(new_time)
-  let hours = new_time.getHours;
-  console.log(hours)
+  var id = document.querySelector("#time")
+  console.log('id is', id);
+
+  let new_time = new Date();
+  console.log(new_time);
+  let hours = new_time.getHours();
+  console.log(hours);
+  let minutes = new_time.getMinutes();
+  console.log(minutes);
+  let sec = new_time.getSeconds();
+  console.log(sec);
+
+  am_pm = hours >= 12 ? 'PM' : 'AM'
+  time.innerHTML = `${hours} <span>:</span> ${minutes} <span>:</span> ${sec}`
+  setTimeout(timer, 1000);
 }
+
+timer()
