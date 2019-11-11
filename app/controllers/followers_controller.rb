21a6ed1cb212e4ddf64642d1508e119f148c21a3
jiaad <abdulziad08@outlook.com>
2019-11-11 23:31:59 +0100

    Follower : refactored

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,6 +1,37 @@
-class FollowersController < ApplicationController
+# frozen_string_literal: true
 
-def new
-	@follow = Follow.new
-end
+class FollowersController < ApplicationController
+  def new
+    @follow = Follow.new
+  end
+
+  def follow
+    @current = current_user
+    @following = Follow.new(user_id: @current.id,
+                            following_id: params[:profile_id])
+    if Follow.check_record(@current.id, params[:profile_id].to_i) == true
+      flash[:danger] = 'cannot follow'
+      redirect_to profile_path(params[:profile_id])
+    elsif @following.save
+      flash[:success] = 'wow you follwed a user'
+      redirect_to profile_path(params[:profile_id])
+    end
+  end
+
+  def unfollow
+    @follow = Follow.find_by(user_id: current_user.id, 
+      following_id: params[:profile_id].to_i)
+    if @follow.destroy
+      redirect_to profile_path(params[:profile_id])
+      flash[:success] = 'wow you un followed'
+    else
+      flash[:danger] = "didn't unfollow"
+    end
+  end
+
+  protected
+
+  def params_follow
+    params.permit(profile_id: @current.id, following_id: :profile_id)
+  end
 end
